ab8e5ae074cb595e4d84c362c8e40db2e1f90058
Stephen <stephen@remine.com>
2019-10-02 08:41:32 -0500

    add knobs, readme, and a11y to storybook

---
+++ .storybook/addons.js
@@ -0,0 +1,3 @@
+import '@storybook/addon-knobs/register';
+import 'storybook-readme/register';
+import '@storybook/addon-a11y/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { addReadme } from 'storybook-readme';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withKnobs);
+addDecorator(addReadme);
+addDecorator(withA11y);
+
+addParameters({
+  readme: {
+      codeTheme: 'github',
+  },
+});
 
 configure(require.context('../src', true, /.stories.(js|jsx|ts|tsx)$/))
